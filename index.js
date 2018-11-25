4f2f9b653498e3bd33ca146ab004f58e69459a4d
Ally Netzel <netzelally@gmail.com>
2018-11-25 02:29:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file.
+// Code your solution in this file
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function (element) { return element.toLowerCase(); })
+}
+
+function nameToAttributes(drivers){
+  return drivers.map(function(element){ 
+    let words = element.split(" ");
+    
+   return { firstName: words[0], lastName: words[1] }
+     })
+}
+
+function attributesToPhrase(drivers){
+  return drivers.map(function(element){
+    return element.name + ` is from ` + element.hometown
+  })
+  
+}
